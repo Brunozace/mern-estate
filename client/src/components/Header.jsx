21d2d74b20f014ccdbe6e0c6fc7e355bed09acb4
JAGER7478 <Brunosanchezacevedo@gmail.com>
2025-07-09 12:48:33 -0400

    db connected + made express backend

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
             <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
                 <Link to="/">
                     <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-                        <span className='text-slate-500'>Acevedo</span>
+                        <span className='text-slate-500'>DobleGestion</span>
                         <span className='text-slate-700'>Propiedades</span>
                     </h1>
                 </Link>
