f1df8d465171e61dc8735c8a77bdc742107f859c
JAGER7478 <Brunosanchezacevedo@gmail.com>
2025-07-10 13:09:47 -0400

    created sign in API for NodeJS backend using JSON Web Token

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,11 +1,75 @@
-import React from 'react'
+import { set } from "mongoose";
+import { useNavigate, Link } from "react-router-dom";
+import React, { useState } from 'react'
+
 
 const SignIn = () => {
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({});
+
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+    console.log(formData);
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+
+      setLoading(true);
+      const res = await fetch("/api/auth/signin", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      console.log(data);
+
+      if (data.success == false) {
+        setLoading(false);
+        setError(data.message);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+      navigate("/")
+
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
+
+  };
+
   return (
-    <div>
-      Inicia Sesion
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Iniciar Sesion</h1>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+
+        <input type="email" placeholder='Email' className='border p-3 rounded-lg' id="email" onChange={handleChange} />
+
+        <input type="password" placeholder='Contraseña' className='border p-3 rounded-lg' id="password" onChange={handleChange} />
+
+        <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-80'>
+          {loading ? "Cargando..." : "Iniciar Sesion"}
+        </button>
+      </form>
+
+      <div className='flex gap-2 mt-5'>
+        <p>No tienes una cuenta?</p>
+        <Link to={"/sign-up"}>
+        <span className='text-blue-700'>Iniciar Sesion</span>
+        </Link>
+      </div>
+      {error && <p className='text-red-500 m'>{error}</p>}
     </div>
   )
 }
 
-export default SignIn
+export default SignIn;
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -74,4 +74,4 @@ const SignUp = () => {
   )
 }
 
-export default SignUp
+export default SignUp;
